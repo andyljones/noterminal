ba2fbfaf803b7cdf283fa6ff53a84bd3e3f729de
Andrew Jones <andyjones.ed@gmail.com>
2019-04-26 20:37:30 +0100

    Initial commit with example code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='noterminal',
+      version='0.1',
+      description='Transient Jupyter notebooks',
+      author='Andy Jones',
+      author_email='andyjones.ed@gmail.com',
+      url='https://github.com/andyljones/noterminal',
+      packages=['noterminal'])
